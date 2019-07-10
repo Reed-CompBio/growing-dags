5e3227d0b3ce0a149c4cd46f9a4d3622435cc4b8
tuncbkose <48298909+tuncbkose@users.noreply.github.com>
2019-07-10 22:56:37 +0000

    Add files via upload

--- DAG/multi_target_dijkstra.py
+++ DAG/multi_target_dijkstra.py
@@ -11,14 +11,28 @@ def pop(heap, finished):
 
 def heap_update(heap, node, dist, current_node, current_path):
     index = 0
+    not_seen = True
     for i in heap:
         if i[1] == node:
+            not_seen = False
             break
         index += 1
     
+    if not_seen:
+        return heap
+     
+    print("Update called for node {}".format(node))
+    print(heap)
+    print("Index at heap: {}".format(index))
     if heap[index][0] > dist:
+        print("Update needed")
         heap[index][0] = dist
-        heap[index][2] = current_path.append(node)
+        new_path = current_path.copy()
+        new_path.append(node)
+        print("Adding node {0} to old path {1}".format(node, current_path))
+        print("New path: {}".format(new_path))
+        heap[index][2] = new_path
+        hq.heapify(heap)
         
     return heap
 
@@ -30,16 +44,26 @@ def multi_target_dijkstra(G, u, vset):
     
     for item in G.nodes():
         path = None
+        dist = math.inf
         if item == u:
             path = [u]
-        hq.heappush(heap, (math.inf, item, path))
+            dist = 0
+        hq.heappush(heap, [dist, item, path])
     
+    print("Heap initialization complete.")
+    print(heap)
     while len(vset) > 0:
         current_node, current_dist, current_path, finished = pop(heap, finished)
+        print("Current node: {}".format(current_node))
+        print("Current dist: {}".format(current_dist))
+        print("Current path: {}".format(current_path))
         if current_node in vset:
             vset.remove(current_node)
         for node in G.successors(current_node):
             dist = current_dist + G[current_node][node]["weight"]
+            print("Calling update for node {}".format(node))
+            print("Before update current path is {}".format(current_path))
             heap = heap_update(heap, node, dist, current_node, current_path)
+            print("\n")
     
     return finished   

9bd7399d606f07e249e376680523a30cd6d2b595
tuncbkose <48298909+tuncbkose@users.noreply.github.com>
2019-07-16 21:01:14 +0000

    Add files via upload

--- DAG/multi_target_dijkstra.py
+++ DAG/multi_target_dijkstra.py
@@ -1,90 +1,44 @@
-#tuncjksta.py
 import heapq as hq
 import math
 
 
-def pop(heap, finished):
-    """
-    A helper function that pops the next element in the priority queue,
-    while also adding it to the discarded pile
-    """
-    dist, node, path = hq.heappop(heap)
-    finished[node] = (dist, path)
-    return node, dist, path, finished
-
-"""
-def heap_update(heap, node, dist, current_node, current_path):
-    # Determine node index in the heap
-    index = 0
-    # Previously popped nodes will not be seen
-    not_seen = True
-    for i in heap:
-        if i[1] == node:
-            not_seen = False
-            break
-        index += 1
-
-    # If node has been previously popped, nothing to update
-    if not_seen:
-        return heap
-
-    # Update only if new distance is shorter
-    if heap[index][0] > dist:
-        heap[index][0] = dist
-        new_path = current_path.copy()
-        new_path.append(node)
-        heap[index][2] = new_path
-        # Re-heapify to restore priority queue
-        hq.heapify(heap)
-
-    return heap
-"""
-
-def heap_update(heap, G, indeces, current_node, current_dist, current_path):
-    for node in G.successors(current_node):
-        try:
-            index = indeces[node]
-        except KeyError:
-            continue
-
-        dist = current_dist + G[current_node][node]["weight"]
-        if heap[index][0] > dist:
-            heap[index][0] = dist
-            new_path = current_path.copy()
-            new_path.append(node)
-            heap[index][2] = new_path
-    hq.heapify(heap)
-    return heap
-
-
 def multi_target_dijkstra(G, u, vset):
+    """
+    Takes in a networkx graph, a starting node u and a set of target nodes vset.
+    Returns a dictionary that has nodes as keys and a tuple of distance, path as values.
+    """
     heap = []   # contains lists of distance, node, path
     finished = {} # node -> (dist, path)
-
+    seen = {} # node -> dist
 
     # Create priority queue entries for each node
     for item in G.nodes():
         path = None
-        dist = 9999999999999999999999999999999999999999999999999999999999999
+        dist = math.inf
+        # Starting node
         if item == u:
             path = [u]
             dist = 0
-        hq.heappush(heap, [dist, item, path])
-
-
-    # Iterate over vset so function will quit when all significant nodes are found
+        hq.heappush(heap, (dist, item, path))
+        seen[item] = dist
+        
+    # Iterate over vset so function will quit when all target nodes are found
     while len(vset) > 0:
-        current_node, current_dist, current_path, finished = pop(heap, finished)
-        indeces = {}
-        for i in range(len(heap)):
-            indeces[heap[i][1]] = i
+        current_dist, current_node, current_path = hq.heappop(heap)
+        if current_node in finished.keys():
+            continue
+        finished[current_node] = (current_dist, current_path)
+
         # A node popping from the priority queue means shortest path to it is found
         if current_node in vset:
             vset.remove(current_node)
-        heap = heap_update(heap, G, indeces, current_node, current_dist, current_path)
-        """
+
         for node in G.successors(current_node):
+            if node in finished.keys():
+                continue
             dist = current_dist + G[current_node][node]["weight"]
-            heap = heap_update(heap, node, dist, current_node, current_path)
-        """
+            if dist < seen[node]:
+                seen[node] = dist
+                hq.heappush(heap, (dist, node, current_path + [node]))
+
     return finished
